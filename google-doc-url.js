52ffc9bf9f2445fb6c9293572a611e7eca1e52f0
ATErickson <erickson@tc.columbia.edu>
2021-07-02 15:26:12 -0400

    Update google-doc-url.js
    Added API

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SSATiAGtTD24_pQ8myOkCQ743vnA6R6rpVbNodagY9g/edit#gid=0;
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDoIUBBOeXLJdgmmtTnuf0Cuo1DIdy6Fgg';
