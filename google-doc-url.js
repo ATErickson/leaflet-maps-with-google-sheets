a4bf429604ec1ff998f846d81f2a5b64cd8daf23
ATErickson <erickson@tc.columbia.edu>
2021-07-02 16:46:06 -0400

    Update google-doc-url.js
    Fixed missing apostrophe in sheets url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SSATiAGtTD24_pQ8myOkCQ743vnA6R6rpVbNodagY9g/edit#gid=0;
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SSATiAGtTD24_pQ8myOkCQ743vnA6R6rpVbNodagY9g/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBbJ-Y1JC1fCWsBr62xGjc1qbXfpblOIa4';
